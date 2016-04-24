b7d1e0ff476d6fea90aabdb8c5eafb2974a22363
Chris Tsoufakis <tsoufakis@gmail.com>
2016-04-24 12:59:29 -0400

    trundling

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 set -eux
 
 scriptdir=$(dirname $0)
+browserify=$scriptdir/node_modules/browserify/bin/cmd.js
 
-browserify -t [ babelify --presets [ react ] ] $scriptdir/src/app.js -o $scriptdir/public/appbundle.js
+$browserify -t [ babelify --presets [ react ] ] $scriptdir/src/app.js -o $scriptdir/public/appbundle.js
