c51c80f0bf524e4fb57f60544ef44886dca34e34
Chris Tsoufakis <chris.tsoufakis@moat.com>
2019-11-28 12:25:13 -0500

    more makefile commands

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
+all: install
+
+install:
+	npm install
+
+build:
+	./build.sh
+
 test:
 	node tests/validate_levels.js ../public/levels.json
 	node tests/test_models.js
 	npm test
+
+clean:
+	rm -rf node_modules
--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ set -eux
 scriptdir=$(dirname $0)
 browserify=$scriptdir/node_modules/browserify/bin/cmd.js
 
-node $scriptdir/validate_levels.js $scriptdir/public/levels.json
 $browserify -t [ babelify --presets [ es2015 react ] ] $scriptdir/src/index.js -o $scriptdir/public/appbundle.js
