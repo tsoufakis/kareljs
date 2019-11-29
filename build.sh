fb0702b8da79d2a4db9b0b79707d958dd19f083f
Chris Tsoufakis <chris.tsoufakis@moat.com>
2019-11-29 15:59:20 -0500

    app running with docker compose

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-set -eux
-
-scriptdir=$(dirname $0)
-browserify=$scriptdir/node_modules/browserify/bin/cmd.js
-
-$browserify -t [ babelify --presets [ env @babel/preset-react ] ] $scriptdir/src/index.js -o $scriptdir/public/appbundle.js
